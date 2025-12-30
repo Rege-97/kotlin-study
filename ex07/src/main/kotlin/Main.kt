89cd30c293c9c015ec57afdc3031784943e77371
Rege-97 <cogus1027@gmail.com>
2025-12-30 18:14:41 +0900

    study: Collection - Map

--- ex07/src/main/kotlin/Main.kt
+++ ex07/src/main/kotlin/Main.kt
@@ -26,14 +26,33 @@ fun main(args: Array<String>) {
     // 순서가 없는 중복되지 않는 요소로 이루어진 자료구조
 
     // 불변 Set
-    val immutableNumSet = setOf(1,1,2,2,3,3,3,4)    // 중복은 제거하고 들어감
+    val immutableNumSet = setOf(1, 1, 2, 2, 3, 3, 3, 4)    // 중복은 제거하고 들어감
     println(immutableNumSet)
 
     // 가변 Set
-    val mutableNumSet = mutableSetOf(1,1,2,3,3,4,5,6)
+    val mutableNumSet = mutableSetOf(1, 1, 2, 3, 3, 4, 5, 6)
     println(mutableNumSet)
     mutableNumSet.add(100)
     mutableNumSet.remove(1)
     println(mutableNumSet)
     println(mutableNumSet.contains(1))
+
+    // 3. Map
+
+    // 불변 Map
+    val immutableMap = mapOf("name" to "Rege", "age" to 29, "height" to 178)    // mapOf() - key to value로 맵 저장
+    println(immutableMap["name"])   // 메서드 없이 []로 접근 가능
+    println(immutableMap["no"])
+
+    // 가변 맵
+    val mutableMap = mutableMapOf("name" to "Rege", "age" to 29, "height" to 178)
+    println(mutableMap["age"])
+
+    mutableMap["age"] = 30
+    println(mutableMap)
+    mutableMap.put("hobby", "coding")
+    mutableMap.remove("name")
+    mutableMap.replace("age", 80)
+
+    println(mutableMap)
 }
