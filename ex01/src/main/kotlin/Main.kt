1611457df8310a4c051511118b961b541231bc39
Rege-97 <cogus1027@gmail.com>
2025-12-30 15:30:32 +0900

    study: 변수와 상수

---
+++ ex01/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    // ; 세미콜론 필요없음
+
+    val pi : Double = 3.14159   // val - 상수
+    println(pi)
+
+    var age : Int = 39  // var - 변수
+    println(age)
+
+    age = 45
+    println(age)
+}
