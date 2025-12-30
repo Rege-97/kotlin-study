75be73bec0d87b7c69f4de5007bcde12ded4150c
Rege-97 <cogus1027@gmail.com>
2025-12-30 16:53:35 +0900

    study: when문

--- ex06/src/main/kotlin/Main.kt
+++ ex06/src/main/kotlin/Main.kt
@@ -5,8 +5,19 @@ fun main(args: Array<String>) {
 //    val examResult = isPass(70)
 //    println("ExamResult : $examResult")
 
-    val is3Contained = is3Contained(5..10)
-    println("is3Contained Result : $is3Contained")
+//    val is3Contained = is3Contained(5..10)
+//    println("is3Contained Result : $is3Contained")
+
+    // 2. when
+    // java의 switch문 대체
+    // when (value) {
+    //    조건1 -> 실행문
+    //    조건2 -> 실행문
+    //    else -> 기본 실행문
+    //}
+
+    val myGrade = getGrade(80)
+    println("My grade : $myGrade")
 }
 
 fun isPass(examScore: Int): Boolean {
@@ -22,5 +33,15 @@ fun is3Contained(intRange: IntRange): Boolean {
         return true
     } else {
         return false
+    }
+}
+
+fun getGrade(score: Int): Char {
+    when (score) {
+        in 0..40 -> return 'D'
+        in 41..70 -> return 'C'
+        in 71..90 -> return 'B'
+        in 91..100 -> return 'A'
+        else -> return 'Z'
     }
 }
