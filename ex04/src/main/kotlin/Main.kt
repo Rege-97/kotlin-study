a1e1c6c381b17da7513bab0cc8edbd44c5c87915
Rege-97 <cogus1027@gmail.com>
2025-12-30 15:55:59 +0900

    study: 스트링템플릿

--- ex04/src/main/kotlin/Main.kt
+++ ex04/src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
+    /**
+     *  String template
+     */
 
+    val price = 1000
+    val tax = 100
+
+    val originalPrice = "The original price is $price"  // $ + 변수명으로 스트링 템플릿 사용가능
+    val taxPrice = "The tax price is ${price + tax}"    // $ + { 수식 } 으로 스트링 템플릿 사용 가능
+
+    println(originalPrice)
+    println(taxPrice)
 }
