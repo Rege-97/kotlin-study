c4a7e847beea2220bb42e619de1bd929ba4d9b6d
Rege-97 <cogus1027@gmail.com>
2025-12-30 16:48:10 +0900

    study: if문

--- ex06/src/main/kotlin/Main.kt
+++ ex06/src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
 fun main(args: Array<String>) {
 
+    // 1. if
+
+//    val examResult = isPass(70)
+//    println("ExamResult : $examResult")
+
+    val is3Contained = is3Contained(5..10)
+    println("is3Contained Result : $is3Contained")
+}
+
+fun isPass(examScore: Int): Boolean {
+    if (examScore >= 60) {
+        return true
+    } else {
+        return false
+    }
+}
+
+fun is3Contained(intRange: IntRange): Boolean {
+    if (intRange.contains(3)) {
+        return true
+    } else {
+        return false
+    }
 }
