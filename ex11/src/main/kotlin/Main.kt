37b0437d14855bac482be26250824e7b257fedde
Rege-97 <cogus1027@gmail.com>
2025-12-31 15:32:10 +0900

    study: 데이터 클래스

--- ex11/src/main/kotlin/Main.kt
+++ ex11/src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
+    val memo = Memo("Go to grocery", "Eggs, Milk, Pokr", false)
 
+    println(memo.toString())
+    println(memo.title)
+    val memo2 = memo.copy(title = "Go to home")
+    println(memo2.toString())
 }
+
+// 데이터 클래스 : 데이터 전달의 목적으로 만드는 클래스
+// data 키워드를 클래스 앞에 붙여서 생성
+// toString(), equals(), hashCode(), copy(), componentN() 등 포함
+data class Memo(val title: String, val content: String, var isDone: Boolean)
