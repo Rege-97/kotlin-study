d9008118fd245ce664e7a7a7e5db4c02bf554866
Rege-97 <cogus1027@gmail.com>
2025-12-31 15:51:11 +0900

    study: 오브젝트와 컴패니언 오브젝트

--- ex13/src/main/kotlin/Main.kt
+++ ex13/src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
 fun main(args: Array<String>) {
+    // 객체 구현 없이 바로 사용
+    println(MyFirstObject.number)
+    MyFirstObject.sayHello()
 
+    println(Dinner.MyCompanionObject.Menu)
+    println(Dinner.eatDinner())
+}
+
+// object : 싱글톤 패턴으로 만들어지는 객체
+object MyFirstObject {
+    val number = 1
+    fun sayHello() {
+        println("Hello")
+    }
+}
+
+// companion object : 클래스 내에 싱글톤 객체 구현
+// static과 동일한 사용
+// 한 클래스에 하나만 생성 가능
+class Dinner {
+    val lunch = "steak"
+    companion object MyCompanionObject {
+        val Menu = "pasta"
+        fun eatDinner() {
+            println("Today's Menu is $Menu")
+        }
+    }
 }
