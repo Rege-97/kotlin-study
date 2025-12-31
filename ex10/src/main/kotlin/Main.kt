ae3b19a21d31ad2dcc39d84b4062eafd357721f8
Rege-97 <cogus1027@gmail.com>
2025-12-31 15:06:25 +0900

    study: 추상 클래스

--- ex10/src/main/kotlin/Main.kt
+++ ex10/src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
 fun main(args: Array<String>) {
+    val overwatch = Overwatch()
 
+    overwatch.startGame()
+    overwatch.printGameName()
+}
+
+// abstract 키워드를 붙여서 추상 클래스 구현
+abstract class Game {
+    fun startGame() {
+        println("Start Game")
+    }
+    // abstract 키워드를 붙여서 추상 함수 구현
+    abstract fun printGameName()
+}
+
+class Overwatch : Game() {
+    override fun printGameName() {
+        println("This is Overwatch")
+    }
 }
